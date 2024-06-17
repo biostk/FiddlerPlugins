cd7e3fc80b38b6f911666048e73ca371e4e292cd
biostk <97348461@qq.com>
2024-06-17 16:04:51 +0800

    0.0.1

--- FiddlerInit.cs
+++ FiddlerInit.cs
@@ -3,13 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Fiddler;
 
 namespace FiddlerPlugins
 {
-    public class FiddlerInit
+    public class FiddlerInit : IAutoTamper
     {
+        void IAutoTamper.AutoTamperRequestAfter(Session oSession)
+        {
+            
+        }
 
+        void IAutoTamper.AutoTamperRequestBefore(Session oSession)
+        {
+            
+        }
 
+        void IAutoTamper.AutoTamperResponseAfter(Session oSession)
+        {
+            
+        }
 
+        void IAutoTamper.AutoTamperResponseBefore(Session oSession)
+        {
+            
+        }
+
+        void IAutoTamper.OnBeforeReturningError(Session oSession)
+        {
+            
+        }
+
+        void IFiddlerExtension.OnBeforeUnload()
+        {
+            
+        }
+
+        void IFiddlerExtension.OnLoad()
+        {
+            
+        }
     }
 }
